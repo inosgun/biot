00879b300bfbb6694aa88b5a27417731241b5d3a
Inos <inosgun@gmail.com>
2022-04-15 23:34:14 +0700

    Initialize sailor-loogies

---
+++ packages/react-app/src/views/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Subgraph } from "./Subgraph";
+export { default as YourShips } from "./YourShips";
+export { default as AddCrew } from "./AddCrew";
